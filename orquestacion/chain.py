684a3f56696e5fba3457fa262a88b4ae5335a521
Stefani Viera <43909428+stefaniv1987@users.noreply.github.com>
2026-06-14 23:15:14 -0300

    Update chain.py

--- orquestacion/chain.py
+++ orquestacion/chain.py
@@ -1,5 +1,7 @@
+from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
+load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o-mini")
 
